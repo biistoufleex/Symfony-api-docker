2010b143f174c7c73c412d5a6422e27149be1f9a
kevin.aubel <kevin.aubel@ouidou.fr>
2023-10-30 18:34:00 +0100

    Séparation des fichiers Api et Applications

--- src/constants/MessageConstants.php
+++ src/constants/MessageConstants.php
@@ -7,4 +7,6 @@ class MessageConstants
     public const PROBLEME_COMMUNICATION_INFOSERVICE_USER='Probleme de communication avec Infoservice pour la recuperation d\'info utilisateur.';
     public const PROBLEME_COMMUNICATION_INFOSERVICE_ETABLISSEMENT='Probleme de communication avec Infoservice pour la recuperation d\'info etablissement.';
     public const PROBLEME_RECUP_OGRANISATION_AUTORISATION='Probleme de recuperation des habilitations de l\'organisation.';
+
+    public const PROBLEME_AUCUN_MESSAGE_USECASE='Aucun message pour le useCase : ';
 }
